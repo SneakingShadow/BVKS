df8f3f83d950af757e8365a1903ed951589c7426
SneakingShadow <thesneakingshadow@gmail.com>
2015-04-14 19:57:42 +0200

    Update ItemBVKSArmor.java

--- src/main/java/com/sneakingshadow/bvks/item/armor/ItemBVKSArmor.java
+++ src/main/java/com/sneakingshadow/bvks/item/armor/ItemBVKSArmor.java
@@ -1,4 +1,65 @@
 package com.sneakingshadow.bvks.item.armor;
 
-public class ItemBVkSArmor {
+import com.sneakingshadow.bvks.creativetab.CreativeTabBVKS;
+import com.sneakingshadow.bvks.reference.Ref;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.Entity;
+import net.minecraft.item.ItemArmor;
+import net.minecraft.item.ItemStack;
+
+public class ItemBVKSArmor extends ItemArmor
+{
+    private String texture;
+    private int armor;
+
+    public ItemBVKSArmor(String textureName, ArmorMaterial material, int armorType)
+    {
+        super(material, 0, armorType);
+        this.setCreativeTab(CreativeTabs.tabAllSearch);
+        this.setCreativeTab(CreativeTabBVKS.tabItem);
+        texture = textureName;
+        armor = armorType;
+    }
+
+    public ItemBVKSArmor(String textureName, ArmorMaterial material, int armorType, int renderIndex)
+    {
+        super(material, renderIndex, armorType);
+        this.setCreativeTab(CreativeTabs.tabAllSearch);
+        this.setCreativeTab(CreativeTabBVKS.tabItem);
+        texture = textureName;
+        armor = armorType;
+    }
+
+    @Override
+    public String getUnlocalizedName()
+    {
+        return String.format("item.%s%s", Ref.RESOURCE_PREFIX, getUnwrappedUnlocalizedName(super.getUnlocalizedName()));
+    }
+
+    @Override
+    public String getUnlocalizedName(ItemStack itemStack) {
+        return String.format("item.%s%s", Ref.RESOURCE_PREFIX, getUnwrappedUnlocalizedName(super.getUnlocalizedName()));
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void registerIcons(IIconRegister iconRegister)
+    {
+        itemIcon = iconRegister.registerIcon(this.getUnlocalizedName().substring(this.getUnlocalizedName().indexOf(".") + 1));
+    }
+
+    protected String getUnwrappedUnlocalizedName(String unlocalizedName)
+    {
+        return unlocalizedName.substring(unlocalizedName.indexOf(".") + 1);
+    }
+
+    @Override
+    public String getArmorTexture(ItemStack stack, Entity entity, int slot, String type)
+    {
+        return Ref.RESOURCE_PREFIX+"textures/armor/" + this.texture + "_" + (this.armor == 2 ? "2" : "1") + ".png";
+    }
+
 }
