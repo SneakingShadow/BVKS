1d5f8af8292ba19c22fc8d3f55c474fbfb835913
SneakingShadow <thesneakingshadow@gmail.com>
2015-06-01 21:03:14 +0200

    removed folder

--- src/main/java/sneakingshadow/bvks/block/BlockDemonAltar.java
+++ src/main/java/sneakingshadow/bvks/block/BlockDemonAltar.java
@@ -1,6 +1,7 @@
 package sneakingshadow.bvks.block;
 
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.world.World;
 import sneakingshadow.bvks.reference.Names;
 import sneakingshadow.bvks.reference.RenderIds;
@@ -33,5 +34,10 @@ public class BlockDemonAltar extends BlockContainerBVKS{
     public boolean isOpaqueCube()
     {
         return false;
+    }
+
+    public AxisAlignedBB getCollisionBoundingBoxFromPool(World p_149668_1_, int p_149668_2_, int p_149668_3_, int p_149668_4_)
+    {
+        return null;
     }
 }
