5f3e3a3bf921519841c5142bcb77f37aca2c4cb5
SneakingShadow <thesneakingshadow@gmail.com>
2015-10-22 12:32:38 +0200

    Bru

--- src/main/java/sneakingshadow/bvks/item/ItemDevilGem.java
+++ src/main/java/sneakingshadow/bvks/item/ItemDevilGem.java
@@ -1,7 +1,11 @@
 package sneakingshadow.bvks.item;
 
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import sneakingshadow.bvks.item.base.ItemBVKS;
 import sneakingshadow.bvks.reference.Names;
+
+import java.util.List;
 
 public class ItemDevilGem extends ItemBVKS
 {
@@ -10,4 +14,10 @@ public class ItemDevilGem extends ItemBVKS
         super();
         this.setUnlocalizedName(Names.Items.DEVIL_GEM);
     }
+
+    @Override
+    public void addInformation(ItemStack itemStack, EntityPlayer player, List list, boolean par4) {
+        list.add("Bru");
+    }
+
 }
