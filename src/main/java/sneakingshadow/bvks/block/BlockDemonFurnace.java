3c3e71491122031fb0fb20deae6aab85a6d0d9ee
SneakingShadow <thesneakingshadow@gmail.com>
2015-12-12 13:01:55 +0100

    Textures

--- src/main/java/sneakingshadow/bvks/block/BlockDemonFurnace.java
+++ src/main/java/sneakingshadow/bvks/block/BlockDemonFurnace.java
@@ -1,10 +1,20 @@
 package sneakingshadow.bvks.block;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.client.renderer.texture.IIconRegister;
+
 public class BlockDemonFurnace extends BlockBVKS {
+
+
 
     public BlockDemonFurnace() {
         super();
     }
 
+    @SideOnly(Side.CLIENT)
+    public void registerBlockIcons(IIconRegister iconRegister)
+    {
 
+    }
 }
